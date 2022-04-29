3f2560012b10f1471a265386e22ebd6349c88e01
field <jsw_777@naver.com>
2022-04-29 14:32:49 +0900

    Initial Project Setting

---
+++ src/test/java/site/orangefield/ajaxtest/AjaxtestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.orangefield.ajaxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
